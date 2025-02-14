7260cb6af9cfde4380787d62e38ea97903b34f13
Veronika272 <mila.2007direkt@gmail.com>
2025-02-14 15:30:21 +0300

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 
-void sayHello() {
-    std::cout << "Hello from utils!" << std::endl;
-}
-
-int add(int a, int b) {
+int addition(int a, int b) {
     return a + b;
 }
 
+int subtraction(int a, int b) {
+    return a - b;
+}
+
+int multiplication(int a, int с) {
+    return a * с;
+}
+
+int main() {
+    int x = 10;
+    int y = 5;
+    int с = 3;
+
+    std::cout << "Сложение: " << сложение(x, y) << std::endl;
+    std::cout << "Вычитание: " << вычитание(x, y) << std::endl;
+    std::cout << "Умножение: " << умножение(x, коэффициент) << std::endl;
+
+    return 0;
+}
